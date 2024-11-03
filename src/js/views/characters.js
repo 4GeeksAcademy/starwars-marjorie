5f5386a9d60ec5e90d8acfa5b94aee376eb4b0a1
Marjoriecus <101658277+Marjoriecus@users.noreply.github.com>
2024-11-03 18:59:25 +0000

    still working

--- src/js/views/characters.js
+++ src/js/views/characters.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Characters = () => {
-    const { store, actions } = useState;
+    const { store, actions } = useState(null);
 
     useEffect(() => {
-        fetch("https://www.swapi.tech/api/people/1")
-            .then((response) => result.JSON)
-            .then((data) => data.result);
+        fetch("https://www.swapi.tech/api/people")
+            .then((response) => response.JSON)
+            .then((data) => actions.setPeopleData(data.result));
 
     })
     return (
@@ -16,27 +16,34 @@ const Characters = () => {
                 <h1> CHARACTERS </h1>
             </div>
 
-        // mapping people - store?
-
-            <div className="list-group list-group-horizontal inline-scroll" style={{ overflowX: "auto" }}></div>
-
-        //card form ////////
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="https://lumiere-a.akamaihd.net/v1/images/the-trickster-flash-raktor_b84df8a2.jpeg?region=953%2C0%2C814%2C813" alt="Card image cap" />
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
+            <div className="list-group list-group-horizontal inline-scroll" style={{ overflowX: "auto" }}>
+                {store.people.map((person) => {
+                    return (
+                        < div key={person.uid}>
+                            <div className="card" style={{ width: "18rem" }}>
+                                <img src="https://lumiere-a.akamaihd.net/v1/images/the-trickster-flash-raktor_b84df8a2.jpeg?region=953%2C0%2C814%2C813" className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">{person.name}</h5>
+                                    <p className="card-text">{person.gender}</p>
+                                    <p className="card-text">{person.hair_color}</p>
+                                    <p className="card-text">{person.eye_color}</p>
+                                    <hr className="my-3" />
+                                    <div className="justify-content-between d-flex align-items-center">
+                                        <link to={"/singlecharacters/" + person.uid}>
+                                            <span className="btn btn-ouliner-black">More</span>
+                                        </link>
+                                        <button className="btn btn-outline-danger" onClick={() => actions.addFavotite(person.name)}>
+                                            <i className="fa fa-heart" />
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            );
+                        </div>
+                    })}
             </div>
-
-
-
         </div>
-    )
-
-
-
-}
+    );
+};
 
 export default Characters;
